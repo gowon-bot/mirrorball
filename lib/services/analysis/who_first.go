42b99a5c5e1e085b2d00374d9b3b48567e321e69
jivison <john@ivison.org>
2021-06-27 21:07:48 -0700

    Fix who first

--- lib/services/analysis/who_first.go
+++ lib/services/analysis/who_first.go
@@ -12,7 +12,6 @@ func (a Analysis) WhoFirstArtist(artist *db.Artist, settings *model.WhoKnowsSett
 	var whoFirst []db.Play
 
 	var sort string
-	aggFunc := "min"
 
 	if whoLast {
 		sort = "desc"
@@ -20,23 +19,22 @@ func (a Analysis) WhoFirstArtist(artist *db.Artist, settings *model.WhoKnowsSett
 		sort = "asc"
 	}
 
-	if whoLast {
-		aggFunc = "max"
-	}
-
 	subquery := db.Db.Model((*db.Play)(nil)).
 		Relation("Track._").
-		ColumnExpr(aggFunc+"(scrobbled_at) as scrobbled_at").
-		Column("user_id").
-		Where("artist_id = ?", artist.ID).
-		Group("user_id")
+		ColumnExpr("Row_number() over(PARTITION BY play.user_id ORDER BY scrobbled_at "+sort+", play.id) AS _rn").
+		Column("play.user_id", "scrobbled_at").
+		Where("artist_id = ?", artist.ID)
 
-	query := db.Db.Model(&whoFirst).
-		Relation("User").
-		Where("(scrobbled_at, play.user_id) IN (?)", subquery).
-		Order("scrobbled_at "+sort, "username desc")
+	subquery = inputparser.CreateParser(subquery).ParseWhoKnowsSettings(settings).GetQuery()
 
-	err := inputparser.CreateParser(query).ParseWhoKnowsSettings(settings).GetQuery().Select()
+	query := db.Db.Model().TableExpr("(?) as play", subquery).
+		Column("play.*").
+		ColumnExpr("u.id as user__id, u.discord_id as user__discord_id, u.username as user__username").
+		Join("JOIN users u ON u.id = play.user_id").
+		Where("play._rn = 1").
+		Order("username desc")
+
+	err := query.Select(&whoFirst)
 
 	if err != nil {
 		return whoFirst, customerrors.DatabaseUnknownError()
