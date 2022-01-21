7c4d05845d0ab690273be796703864e3dd4fcdf2
jivison <john@ivison.org>
2022-01-21 14:01:32 -0800

    Add logs to deploy

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
+echo "Pulling latest code from Github"
 git pull
 
+echo "Building image"
 docker build . -t mirrorball
 
+echo "Restarting service"
 cd $1
 docker-compose up --force-recreate --no-deps -d mirrorball
